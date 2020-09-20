0c7150b8131d414667bef99dc262fd30ae2e01b4
larinnim <larinnimalheiros@gmail.com>
2020-09-20 11:58:17 -0400

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 void main() => runApp(MyApp());
 
@@ -35,6 +36,8 @@ class _MyHomePageState extends State<MyHomePage> {
   Marker marker;
   Circle circle;
   GoogleMapController _controller;
+  Map<PolylineId, Polyline> _mapPolylines = {};
+  int _polylineIdCounter = 1;
 
   static final CameraPosition initialLocation = CameraPosition(
     target: LatLng(46.520444, -80.954180),
@@ -42,8 +45,44 @@ class _MyHomePageState extends State<MyHomePage> {
   );
 
   Future<Uint8List> getMarker() async {
-    ByteData byteData = await DefaultAssetBundle.of(context).load("assets/dog.png");
+    ByteData byteData =
+        await DefaultAssetBundle.of(context).load("assets/dog.png");
     return byteData.buffer.asUint8List();
+  }
+
+  void add() {
+    final String polylineIdVal = 'polyline_id_$_polylineIdCounter';
+    _polylineIdCounter++;
+    final PolylineId polylineId = PolylineId(polylineIdVal);
+
+    final Polyline polyline = Polyline(
+      polylineId: polylineId,
+      consumeTapEvents: true,
+      color: Colors.red,
+      width: 5,
+      points: _createPoints(),
+    );
+
+    setState(() {
+      _mapPolylines[polylineId] = polyline;
+    });
+  }
+
+  List<LatLng> _createPoints() {
+    final List<LatLng> points = <LatLng>[];
+    points.add(LatLng(46.5203678, -80.9545719));
+    points.add(LatLng(46.52077, -80.95457));
+    points.add(LatLng(46.52183, -80.95457));
+    points.add(LatLng(46.52208, -80.95458));
+    points.add(LatLng(46.5220848, -80.9545758));
+    points.add(LatLng(46.52207, -80.95596));
+    points.add(LatLng(46.52207, -80.95763));
+    points.add(LatLng(46.52208, -80.95819));
+    points.add(LatLng(46.52207, -80.95842));
+    points.add(LatLng(46.52208, -80.95888));
+    points.add(LatLng(46.52208, -80.95901));
+    points.add(LatLng(46.52207, -80.9592));
+    return points;
   }
 
   void updateMarkerAndCircle(LocationData newLocalData, Uint8List imageData) {
@@ -58,6 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
           flat: true,
           anchor: Offset(0.5, 0.5),
           icon: BitmapDescriptor.fromBytes(imageData));
+      add();
       /*
       circle = Circle(
           circleId: CircleId("car"),
@@ -72,7 +112,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void getCurrentLocation() async {
     try {
-
       Uint8List imageData = await getMarker();
       var location = await _locationTracker.getLocation();
 
@@ -82,18 +121,18 @@ class _MyHomePageState extends State<MyHomePage> {
         _locationSubscription.cancel();
       }
 
-
-      _locationSubscription = _locationTracker.onLocationChanged().listen((newLocalData) {
+      _locationSubscription =
+          _locationTracker.onLocationChanged().listen((newLocalData) {
         if (_controller != null) {
-          _controller.animateCamera(CameraUpdate.newCameraPosition(new CameraPosition(
-              bearing: 192.8334901395799,
-              target: LatLng(newLocalData.latitude, newLocalData.longitude),
-              tilt: 0,
-              zoom: 18.00)));
+          _controller.animateCamera(CameraUpdate.newCameraPosition(
+              new CameraPosition(
+                  bearing: 192.8334901395799,
+                  target: LatLng(newLocalData.latitude, newLocalData.longitude),
+                  tilt: 0,
+                  zoom: 18.00)));
           updateMarkerAndCircle(newLocalData, imageData);
         }
       });
-
     } on PlatformException catch (e) {
       if (e.code == 'PERMISSION_DENIED') {
         debugPrint("Permission Denied");
@@ -120,10 +159,10 @@ class _MyHomePageState extends State<MyHomePage> {
         initialCameraPosition: initialLocation,
         markers: Set.of((marker != null) ? [marker] : []),
         circles: Set.of((circle != null) ? [circle] : []),
+        polylines: Set<Polyline>.of(_mapPolylines.values),
         onMapCreated: (GoogleMapController controller) {
           _controller = controller;
         },
-
       ),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.location_searching),
