381a699ab0c61915f0725233a187bab25b8f16a3
larinnim <larinnimalheiros@gmail.com>
2020-09-21 20:08:55 -0400

    first SignIN screen

--- lib/Screens/Authenticate/Register.dart
+++ lib/Screens/Authenticate/Register.dart
@@ -4,45 +4,111 @@ import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
+  final Function cancelBackToHome;
+
+  Register({this.cancelBackToHome});
+
   @override
   _RegisterState createState() => _RegisterState();
-  }
-  
+}
+
 class _RegisterState extends State<Register> {
   bool _termsAgreed = false;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(children: <Widget>[
-        Text('CREATE YOUR LOGIN', style: TextStyle(color: Colors.white, fontSize: 26.0, fontWeight: FontWeight.w600)),
-        SizedBox(height: 12.0),
-        TextField(decoration: InputDecoration(hintText: 'Username', hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)), focusColor: Colors.white, focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)), enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white))), style: TextStyle(color: Colors.white, fontSize: 22.0),),
-        SizedBox(height: 20.0),
-        TextField(obscureText: true, decoration: InputDecoration(hintText: 'Password', hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)), focusColor: Colors.white, focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)), enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white))), style: TextStyle(color: Colors.white, fontSize: 22.0),),
-        SizedBox(height: 12.0),
-        TextField(obscureText: true, decoration: InputDecoration(hintText: 'Re-enter Password', hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)), focusColor: Colors.white, focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)), enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white))), style: TextStyle(color: Colors.white, fontSize: 22.0),),
-        SizedBox(height: 12.0),
-        Row(children: <Widget>[
-          Checkbox(activeColor: Colors.orange, value: _termsAgreed, onChanged: (newValue) {
-            setState(() {
-              _termsAgreed = newValue;
-            });
-          },),   
-          // SizedBox(width: 38.0),
-          Text('Agreed to Terms & Condition', style: TextStyle(color: Colors.white, fontSize: 16.0))
-        ]
-        ),
-        SizedBox(width: 20.0),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-            Text('CANCEL', style: TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),),
+      padding: EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
+        children: <Widget>[
+          Text('CREATE YOUR LOGIN',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.w600)),
+          SizedBox(height: 12.0),
+          TextField(
+            decoration: InputDecoration(
+                hintText: 'Username',
+                hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)),
+                focusColor: Colors.white,
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white))),
+            style: TextStyle(color: Colors.white, fontSize: 22.0),
+          ),
+          SizedBox(height: 20.0),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+                hintText: 'Password',
+                hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)),
+                focusColor: Colors.white,
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white))),
+            style: TextStyle(color: Colors.white, fontSize: 22.0),
+          ),
+          SizedBox(height: 12.0),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+                hintText: 'Re-enter Password',
+                hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)),
+                focusColor: Colors.white,
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white))),
+            style: TextStyle(color: Colors.white, fontSize: 22.0),
+          ),
+          SizedBox(height: 12.0),
+          Row(children: <Widget>[
+            Checkbox(
+              activeColor: Colors.orange,
+              value: _termsAgreed,
+              onChanged: (newValue) {
+                setState(() {
+                  _termsAgreed = newValue;
+                });
+              },
+            ),
+            // SizedBox(width: 38.0),
+            Text('Agreed to Terms & Condition',
+                style: TextStyle(color: Colors.white, fontSize: 16.0))
+          ]),
+          SizedBox(width: 20.0),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            InkWell(
+              onTap: () {
+                widget.cancelBackToHome();
+              },
+              child: Text(
+                'CANCEL',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
             SizedBox(width: 38.0),
-            Container(padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 34.0), decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(30.0)), child: Text('SAVE', style: TextStyle(color: Colors.red, fontSize: 20, fontWeight: FontWeight.bold),)
-            ) 
-          ]
-        )
-      ],),
+            Container(
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 34.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30.0)),
+                child: Text(
+                  'SAVE',
+                  style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ))
+          ])
+        ],
+      ),
     );
   }
 }
--- lib/Screens/Authenticate/Sign_in_registered.dart
+++ lib/Screens/Authenticate/Sign_in_registered.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SignInRegistered extends StatefulWidget {
+  final Function gotoSignUp;
+  SignInRegistered({this.gotoSignUp});
+
   @override
   _SignInRegisteredState createState() => _SignInRegisteredState();
 }
@@ -10,57 +13,122 @@ class _SignInRegisteredState extends State<SignInRegistered> {
   bool _rememberPassword = false;
   @override
   Widget build(BuildContext context) {
-      return Container(
-        child: Column(children: <Widget>[
-          Text('SIGN IN', style: TextStyle(color: Colors.white, fontSize: 26.0, fontWeight: FontWeight.w600)),
-          SizedBox(height: 12.0),
-          TextField(decoration: InputDecoration(hintText: 'Enter Email', hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)), focusColor: Colors.white, focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)), enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white))), style: TextStyle(color: Colors.white, fontSize: 22.0),),
-          SizedBox(height: 20.0),
-          TextField(obscureText: true, decoration: InputDecoration(hintText: 'Password', hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)), focusColor: Colors.white, focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)), enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white))), style: TextStyle(color: Colors.white, fontSize: 22.0),),
-          SizedBox(height: 12.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
             children: <Widget>[
-            Checkbox(activeColor: Colors.orange, value: _rememberPassword, onChanged: (newValue) {
-              setState(() {
-                _rememberPassword = newValue;
-              });
-            },),   
-            Text('Remember Password', style: TextStyle(color: Colors.white, fontSize: 16.0))
-          ]
+              Text('SIGN IN',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 26.0,
+                      fontWeight: FontWeight.w600)),
+              SizedBox(height: 12.0),
+              TextField(
+                decoration: InputDecoration(
+                    hintText: 'Enter Email',
+                    hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)),
+                    focusColor: Colors.white,
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white))),
+                style: TextStyle(color: Colors.white, fontSize: 22.0),
+              ),
+              SizedBox(height: 20.0),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)),
+                    focusColor: Colors.white,
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white))),
+                style: TextStyle(color: Colors.white, fontSize: 22.0),
+              ),
+              SizedBox(height: 12.0),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Checkbox(
+                      activeColor: Colors.orange,
+                      value: _rememberPassword,
+                      onChanged: (newValue) {
+                        setState(() {
+                          _rememberPassword = newValue;
+                        });
+                      },
+                    ),
+                    Text('Remember Password',
+                        style: TextStyle(color: Colors.white, fontSize: 16.0))
+                  ]),
+              SizedBox(height: 20.0),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                        // width: double.infinity,
+                        padding: EdgeInsets.symmetric(
+                            vertical: 16.0, horizontal: 34.0),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30.0)),
+                        child: Text(
+                          'LOG IN',
+                          style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ))
+                  ]),
+              SizedBox(height: 20.0),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: <
+                  Widget>[
+                Container(
+                    padding: EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30.0)),
+                    child: Icon(FontAwesomeIcons.google, color: Colors.red)),
+                SizedBox(width: 38.0),
+                Container(
+                    padding: EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30.0)),
+                    child: Icon(FontAwesomeIcons.facebookF, color: Colors.red)),
+              ]),
+              SizedBox(height: 20.0),
+              Text('FORGOT PASSWORD?',
+                  style: TextStyle(
+                      color: Colors.white,
+                      decoration: TextDecoration.underline,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold)),
+            ],
           ),
-          SizedBox(height: 20.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[  
-              Container(
-                // width: double.infinity,
-                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 34.0), decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(30.0)), child: Text('LOG IN', style: TextStyle(color: Colors.red, fontSize: 20, fontWeight: FontWeight.bold),)
-              ) 
-            ]
-          ),
-          SizedBox(height: 20.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(20.0), decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(30.0)), 
-                child: Icon(FontAwesomeIcons.google, color: Colors.red)),
-              SizedBox(width: 38.0),
-              Container(
-                padding: EdgeInsets.all(20.0), decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(30.0)), 
-                child: Icon(FontAwesomeIcons.facebookF, color: Colors.red)), 
-            ]
-          ),
-          SizedBox(height: 20.0),
-          Text('FORGOT PASSWORD?', style: TextStyle(color: Colors.white, decoration: TextDecoration.underline, fontSize: 16.0, fontWeight: FontWeight.bold)),
-          Column(children: <Widget>[
-            Container(
-            color: Colors.black.withOpacity(0.2), child:
-            Text('DON\'T HAVE AN ACCOUNT? SIGN UP', style: TextStyle(color: Colors.white, decoration: TextDecoration.underline, fontSize: 16.0, fontWeight: FontWeight.bold))
-          )
-          ]),
-        ],
         ),
-      );
-
+        InkWell(
+          onTap: () {
+            widget.gotoSignUp();
+          },
+          child: Container(
+              padding: EdgeInsets.all(10.0),
+              width: double.infinity,
+              color: Colors.black.withOpacity(0.2),
+              child: Text('DON\'T HAVE AN ACCOUNT? SIGN UP',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.orange,
+                      decoration: TextDecoration.underline,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w700))),
+        ),
+      ],
+    );
   }
 }
