db3c7d75ec4e46aa44f9824953a9fe08cc258d17
larinnim <larinnimalheiros@gmail.com>
2020-09-22 21:35:23 -0400

    adding facebook login

--- lib/Screens/Authenticate/home_sigin_widget.dart
+++ lib/Screens/Authenticate/home_sigin_widget.dart
@@ -1,12 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/semantics.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
 
 class HomeSignInWidget extends StatelessWidget {
   final Function gotoSignUp;
   final Function gotoSignIn;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   HomeSignInWidget({this.gotoSignUp, this.gotoSignIn});
+
+  void _signInFacebook() async {
+    final facebookLogin = FacebookLogin();
+    final result = await facebookLogin.logIn(['email']);
+    final token = result.accessToken.token;
+    final graphResponse = await http.get(
+        'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email&access_token=${token}');
+    print(graphResponse.body);
+    // if (result.status == FacebookLoginStatus.loggedIn) {
+    //   final credential = FacebookAuthProvider.getCredential(token)
+    // }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -32,23 +49,28 @@ class HomeSignInWidget extends StatelessWidget {
         SizedBox(
           height: 20.0,
         ),
-        Container(
-            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(30.0)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Icon(FontAwesomeIcons.facebookF, color: Colors.red, size: 30.0),
-                Text(
-                  ' | Sign in with Facebook',
-                  style: TextStyle(
-                      color: Colors.red,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0),
-                )
-              ],
-            )),
+        InkWell(
+          onTap: () {
+            _signInFacebook();
+          },
+          child: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(30.0)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(FontAwesomeIcons.facebookF, color: Colors.red, size: 30.0),
+              Text(
+                ' | Sign in with Facebook',
+                style: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0),
+              )
+            ],
+          )),
+        ),
         SizedBox(
           height: 20.0,
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
 import 'dart:async';
 import 'dart:typed_data';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_maps/Screens/Home/wrapper.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Locate My Pet',
       theme: ThemeData(
         primaryColor: Colors.white,
