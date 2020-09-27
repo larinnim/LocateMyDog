3c40029a4248c138390bcf080b825ca3bd80d7bc
larinnim <larinnimalheiros@gmail.com>
2020-09-27 12:27:10 -0400

    w

--- lib/Screens/Authenticate/Register.dart
+++ lib/Screens/Authenticate/Register.dart
@@ -5,7 +5,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_maps/Models/user.dart';
 import 'package:flutter_maps/Screens/Profile/profile.dart';
+import 'package:flutter_maps/Services/database.dart';
 
 class Register extends StatefulWidget {
   final Function cancelBackToHome;
@@ -19,21 +21,24 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   bool _termsAgreed = false;
   bool saveAttempted = false;
-  String email, password, passwordConfirm;
+  String email, password, passwordConfirm, dogname, dogowner, dogbreed, dogage;
   final formKey = GlobalKey<FormState>();
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   void _createUser({String email, String pw}) {
     _auth
         .createUserWithEmailAndPassword(email: email, password: pw)
-        .then((authResult) {
+        .then((authResult) async {
+      authResult.user != null
+        ? AppUser(authResult.user.uid, displayName: authResult.user.displayName)
+        : null;
+      // create a new document for the user with the uid
+      await DatabaseService(uid: authResult.user.uid)
+          .updateUserData(dogname, dogowner, dogbreed, 100);
       print('yay! ${authResult.user}');
       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
-        return Container(
-            color: Colors.yellow,
-            child:
-            ProfileScreen());
-            //  Text('Welcome ${authResult.user.email}'));
+        return Container(color: Colors.yellow, child: ProfileScreen());
+        //  Text('Welcome ${authResult.user.email}'));
       }));
     }).catchError((err) {
       print(err);
@@ -62,164 +67,256 @@ class _RegisterState extends State<Register> {
   Widget build(BuildContext context) {
     return Form(
       key: formKey,
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          children: <Widget>[
-            Text('CREATE YOUR LOGIN',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 26.0,
-                    fontWeight: FontWeight.w600)),
-            SizedBox(height: 12.0),
-            TextFormField(
-              autovalidate: saveAttempted,
-              onChanged: (textValue) {
-                setState(() {
-                  email = textValue;
-                });
-              },
-              validator: (emailValue) {
-                if (emailValue.isEmpty) {
-                  return 'This field is mandatory';
-                }
-
-                String p =
-                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-                RegExp regExp = new RegExp(p);
-
-                if (regExp.hasMatch(emailValue)) {
-                  //The email is valid
-                  return null;
-                }
-
-                return 'This is not a valid email';
-              },
-              decoration: InputDecoration(
-                  errorStyle: TextStyle(color: Colors.white),
-                  hintText: 'Enter Email',
-                  hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)),
-                  focusColor: Colors.white,
-                  focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white)),
-                  enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white))),
-              style: TextStyle(color: Colors.white, fontSize: 22.0),
-            ),
-            SizedBox(height: 20.0),
-            TextFormField(
-              autovalidate: saveAttempted,
-              onChanged: (textValue) {
-                setState(() {
-                  password = textValue;
-                });
-              },
-              validator: (pwValue) {
-                if (pwValue.isEmpty) {
-                  return 'This field is mandatory';
-                }
-                if (pwValue.length < 8) {
-                  return 'Passoword must be at least 8 characters';
-                }
-                return null;
-              },
-              obscureText: true,
-              decoration: InputDecoration(
-                  errorStyle: TextStyle(color: Colors.white),
-                  hintText: 'Password',
-                  hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)),
-                  focusColor: Colors.white,
-                  focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white)),
-                  enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white))),
-              style: TextStyle(color: Colors.white, fontSize: 22.0),
-            ),
-            SizedBox(height: 12.0),
-            TextFormField(
-              autovalidate: saveAttempted,
-              onChanged: (textValue) {
-                setState(() {
-                  passwordConfirm = textValue;
-                });
-              },
-              validator: (pwConfirmValue) {
-                if (pwConfirmValue.isEmpty) {
-                  return 'This field is mandatory';
-                }
-                if (pwConfirmValue != password) {
-                  return 'Passwords must match';
-                }
-                return null;
-              },
-              obscureText: true,
-              decoration: InputDecoration(
-                  errorStyle: TextStyle(color: Colors.white),
-                  hintText: 'Re-enter Password',
-                  hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)),
-                  focusColor: Colors.white,
-                  focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white)),
-                  enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white))),
-              style: TextStyle(color: Colors.white, fontSize: 22.0),
-            ),
-            SizedBox(height: 12.0),
-            Row(children: <Widget>[
-              Checkbox(
-                activeColor: Colors.orange,
-                value: _termsAgreed,
-                onChanged: (newValue) {
+      child: SingleChildScrollView(
+          child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                autovalidate: saveAttempted,
+                onChanged: (textValue) {
                   setState(() {
-                    _termsAgreed = newValue;
+                    dogname = textValue;
                   });
                 },
-              ),
-              // SizedBox(width: 38.0),
-              Text('Agreed to Terms & Condition',
-                  style: TextStyle(color: Colors.white, fontSize: 16.0))
-            ]),
-            SizedBox(width: 20.0),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              InkWell(
-                onTap: () {
-                  widget.cancelBackToHome();
-                },
-                child: Text(
-                  'CANCEL',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-              SizedBox(width: 38.0),
-              InkWell(
-                onTap: () {
-                  setState(() {
-                    saveAttempted = true;
-                  });
-                  if (formKey.currentState.validate()) {
-                    formKey.currentState.save();
-                    _createUser(email: email, pw: password);
+                validator: (dogNameVal) {
+                  if (dogNameVal.isEmpty) {
+                    return 'This field is mandatory';
                   }
                 },
-                child: Container(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 16.0, horizontal: 34.0),
-                    decoration: BoxDecoration(
+                decoration: InputDecoration(
+                    errorStyle: TextStyle(color: Colors.white),
+                    hintText: 'Dog Name: ',
+                    hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)),
+                    focusColor: Colors.white,
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white))),
+                style: TextStyle(color: Colors.white, fontSize: 22.0),
+              ),
+              SizedBox(height: 12.0),
+              TextFormField(
+                autovalidate: saveAttempted,
+                onChanged: (textValue) {
+                  setState(() {
+                    dogname = textValue;
+                  });
+                },
+                validator: (dogOwnerVal) {
+                  if (dogOwnerVal.isEmpty) {
+                    return 'This field is mandatory';
+                  }
+                },
+                decoration: InputDecoration(
+                    errorStyle: TextStyle(color: Colors.white),
+                    hintText: 'Dog Owner: ',
+                    hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)),
+                    focusColor: Colors.white,
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white))),
+                style: TextStyle(color: Colors.white, fontSize: 22.0),
+              ),
+              SizedBox(height: 12.0),
+              TextFormField(
+                autovalidate: saveAttempted,
+                onChanged: (textValue) {
+                  setState(() {
+                    dogname = textValue;
+                  });
+                },
+                validator: (dogBreedVal) {
+                  if (dogBreedVal.isEmpty) {
+                    return 'This field is mandatory';
+                  }
+                },
+                decoration: InputDecoration(
+                    errorStyle: TextStyle(color: Colors.white),
+                    hintText: 'Dog Breed: ',
+                    hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)),
+                    focusColor: Colors.white,
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white))),
+                style: TextStyle(color: Colors.white, fontSize: 22.0),
+              ),
+              SizedBox(height: 12.0),
+              TextFormField(
+                autovalidate: saveAttempted,
+                onChanged: (textValue) {
+                  setState(() {
+                    dogname = textValue;
+                  });
+                },
+                validator: (dogBreedVal) {
+                  if (dogBreedVal.isEmpty) {
+                    return 'This field is mandatory';
+                  }
+                },
+                decoration: InputDecoration(
+                    errorStyle: TextStyle(color: Colors.white),
+                    hintText: 'Dog Age: ',
+                    hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)),
+                    focusColor: Colors.white,
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white))),
+                style: TextStyle(color: Colors.white, fontSize: 22.0),
+              ),
+              SizedBox(height: 12.0),
+              TextFormField(
+                autovalidate: saveAttempted,
+                onChanged: (textValue) {
+                  setState(() {
+                    email = textValue;
+                  });
+                },
+                validator: (emailValue) {
+                  if (emailValue.isEmpty) {
+                    return 'This field is mandatory';
+                  }
+
+                  String p =
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                  RegExp regExp = new RegExp(p);
+
+                  if (regExp.hasMatch(emailValue)) {
+                    //The email is valid
+                    return null;
+                  }
+
+                  return 'This is not a valid email';
+                },
+                decoration: InputDecoration(
+                    errorStyle: TextStyle(color: Colors.white),
+                    hintText: 'Enter Email',
+                    hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)),
+                    focusColor: Colors.white,
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white))),
+                style: TextStyle(color: Colors.white, fontSize: 22.0),
+              ),
+              SizedBox(height: 20.0),
+              TextFormField(
+                autovalidate: saveAttempted,
+                onChanged: (textValue) {
+                  setState(() {
+                    password = textValue;
+                  });
+                },
+                validator: (pwValue) {
+                  if (pwValue.isEmpty) {
+                    return 'This field is mandatory';
+                  }
+                  if (pwValue.length < 8) {
+                    return 'Passoword must be at least 8 characters';
+                  }
+                  return null;
+                },
+                obscureText: true,
+                decoration: InputDecoration(
+                    errorStyle: TextStyle(color: Colors.white),
+                    hintText: 'Password',
+                    hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)),
+                    focusColor: Colors.white,
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white))),
+                style: TextStyle(color: Colors.white, fontSize: 22.0),
+              ),
+              SizedBox(height: 12.0),
+              TextFormField(
+                autovalidate: saveAttempted,
+                onChanged: (textValue) {
+                  setState(() {
+                    passwordConfirm = textValue;
+                  });
+                },
+                validator: (pwConfirmValue) {
+                  if (pwConfirmValue.isEmpty) {
+                    return 'This field is mandatory';
+                  }
+                  if (pwConfirmValue != password) {
+                    return 'Passwords must match';
+                  }
+                  return null;
+                },
+                obscureText: true,
+                decoration: InputDecoration(
+                    errorStyle: TextStyle(color: Colors.white),
+                    hintText: 'Re-enter Password',
+                    hintStyle: TextStyle(color: Colors.white.withOpacity(0.6)),
+                    focusColor: Colors.white,
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white))),
+                style: TextStyle(color: Colors.white, fontSize: 22.0),
+              ),
+              SizedBox(height: 12.0),
+              Row(children: <Widget>[
+                Checkbox(
+                  activeColor: Colors.orange,
+                  value: _termsAgreed,
+                  onChanged: (newValue) {
+                    setState(() {
+                      _termsAgreed = newValue;
+                    });
+                  },
+                ),
+                // SizedBox(width: 38.0),
+                Text('Agreed to Terms & Condition',
+                    style: TextStyle(color: Colors.white, fontSize: 16.0))
+              ]),
+              SizedBox(width: 20.0),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+                InkWell(
+                  onTap: () {
+                    widget.cancelBackToHome();
+                  },
+                  child: Text(
+                    'CANCEL',
+                    style: TextStyle(
                         color: Colors.white,
-                        borderRadius: BorderRadius.circular(30.0)),
-                    child: Text(
-                      'SAVE',
-                      style: TextStyle(
-                          color: Colors.red,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    )),
-              )
-            ])
-          ],
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(width: 38.0),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      saveAttempted = true;
+                    });
+                    if (formKey.currentState.validate()) {
+                      formKey.currentState.save();
+                      _createUser(email: email, pw: password);
+                    }
+                  },
+                  child: Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 16.0, horizontal: 34.0),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30.0)),
+                      child: Text(
+                        'SAVE',
+                        style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      )),
+                )
+              ])
+            ],
+          ),
         ),
       ),
     );
