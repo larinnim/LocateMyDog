a3b1fbfd24a36f82da8c7f279bb0916c752f7eb5
larinnim <larinnimalheiros@gmail.com>
2020-10-04 18:59:35 -0400

    w

--- lib/Screens/Profile/MapLocation.dart
+++ lib/Screens/Profile/MapLocation.dart
@@ -2,11 +2,14 @@ import 'dart:async';
 import 'dart:io' show Platform;
 import 'dart:typed_data';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_maps/Screens/Profile/profile.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:local_auth/local_auth.dart';
 import 'package:location/location.dart';
@@ -22,8 +25,9 @@ class MapLocation extends StatefulWidget {
   _MapLocationState createState() => _MapLocationState();
 }
 
-class _MapLocationState extends State<MapLocation> with AutomaticKeepAliveClientMixin{
-   StreamSubscription _locationSubscription;
+class _MapLocationState extends State<MapLocation>
+    with AutomaticKeepAliveClientMixin {
+  StreamSubscription _locationSubscription;
   Location _locationTracker = Location();
   Marker marker;
   Circle circle;
@@ -31,13 +35,15 @@ class _MapLocationState extends State<MapLocation> with AutomaticKeepAliveClient
   Map<PolylineId, Polyline> _mapPolylines = {};
   int _polylineIdCounter = 1;
   final List<LatLng> points = <LatLng>[];
+  final _firestore = FirebaseFirestore.instance;
+  // FirebaseStorage firestore = FirebaseStorage.instance;
+  Geoflutterfire geo = Geoflutterfire();
 
-@override
+  @override
   bool get wantKeepAlive => true;
 
   void _onMapCreated(GoogleMapController controller) {
-    if( _controller == null )
-      _controller = controller;
+    if (_controller == null) _controller = controller;
   }
 
   static final CameraPosition initialLocation = CameraPosition(
@@ -58,12 +64,11 @@ class _MapLocationState extends State<MapLocation> with AutomaticKeepAliveClient
     points.add(LatLng(newLocalData.latitude, newLocalData.longitude));
 
     final Polyline polyline = Polyline(
-      polylineId: polylineId,
-      consumeTapEvents: true,
-      color: Colors.red,
-      width: 5,
-      points: points
-    );
+        polylineId: polylineId,
+        consumeTapEvents: true,
+        color: Colors.red,
+        width: 5,
+        points: points);
 
     setState(() {
       _mapPolylines[polylineId] = polyline;
@@ -116,6 +121,11 @@ class _MapLocationState extends State<MapLocation> with AutomaticKeepAliveClient
                   zoom: 18.00)));
           updateMarkerAndCircle(newLocalData, imageData);
           updatePolygon(newLocalData);
+          //  GeoFirePoint point =
+          // geo.point(latitude: newLocalData.latitude, longitude: newLocalData.longitude);
+          // _firestore
+          // .collection('locations')
+          // .add({'position': point.data, 'name': 'Yay I can be queried!'});
         }
       });
     } on PlatformException catch (e) {
@@ -133,122 +143,36 @@ class _MapLocationState extends State<MapLocation> with AutomaticKeepAliveClient
     super.dispose();
   }
 
-  // StreamSubscription _locationSubscription;
-  // Location _locationTracker = Location();
-  // Marker marker;
-  // Circle circle;
-  // GoogleMapController _controller;
-
-  // static final CameraPosition initialLocation = CameraPosition(
-  //   target: LatLng(37.42796133580664, -122.085749655962),
-  //   zoom: 14.4746,
-  // );
-
-  // Future<Uint8List> getMarker() async {
-  //   ByteData byteData = await DefaultAssetBundle.of(context).load("assets/car_icon.png");
-  //   return byteData.buffer.asUint8List();
+  // Future<DocumentReference> _addGeoPoint() async {
+  //   var pos = getCurrentLocation();
+  //   GeoFirePoint point =
+  //       geo.point(latitude: pos.latitude, longitude: pos.longitude);
+  //   return _firestore
+  //       .collection('locations')
+  //       .add({'position': point.data, 'name': 'Yay I can be queried!'});
   // }
 
-  // void updateMarkerAndCircle(LocationData newLocalData, Uint8List imageData) {
-  //   LatLng latlng = LatLng(newLocalData.latitude, newLocalData.longitude);
-  //   this.setState(() {
-  //     marker = Marker(
-  //         markerId: MarkerId("home"),
-  //         position: latlng,
-  //         rotation: newLocalData.heading,
-  //         draggable: false,
-  //         zIndex: 2,
-  //         flat: true,
-  //         anchor: Offset(0.5, 0.5),
-  //         icon: BitmapDescriptor.fromBytes(imageData));
-  //     circle = Circle(
-  //         circleId: CircleId("car"),
-  //         radius: newLocalData.accuracy,
-  //         zIndex: 1,
-  //         strokeColor: Colors.blue,
-  //         center: latlng,
-  //         fillColor: Colors.blue.withAlpha(70));
-  //   });
-  // }
-
-  // void getCurrentLocation() async {
-  //   try {
-
-  //     Uint8List imageData = await getMarker();
-  //     var location = await _locationTracker.getLocation();
-
-  //     updateMarkerAndCircle(location, imageData);
-
-  //     if (_locationSubscription != null) {
-  //       _locationSubscription.cancel();
-  //     }
-
-
-  //     _locationSubscription = _locationTracker.onLocationChanged().listen((newLocalData) {
-  //       if (_controller != null) {
-  //         _controller.animateCamera(CameraUpdate.newCameraPosition(new CameraPosition(
-  //             bearing: 192.8334901395799,
-  //             target: LatLng(newLocalData.latitude, newLocalData.longitude),
-  //             tilt: 0,
-  //             zoom: 18.00)));
-  //         updateMarkerAndCircle(newLocalData, imageData);
-  //       }
-  //     });
-
-  //   } on PlatformException catch (e) {
-  //     if (e.code == 'PERMISSION_DENIED') {
-  //       debugPrint("Permission Denied");
-  //     }
-  //   }
-  // }
-
-  // @override
-  // void dispose() {
-  //   if (_locationSubscription != null) {
-  //     _locationSubscription.cancel();
-  //   }
-  //   super.dispose();
-  // }
-  
   @override
   Widget build(BuildContext context) {
     final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-    // return ProfileScreen();
-// return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Google Maps location"),
-//       ),
-//       body: GoogleMap(
-//         mapType: MapType.hybrid,
-//         initialCameraPosition: initialLocation,
-//         markers: Set.of((marker != null) ? [marker] : []),
-//         circles: Set.of((circle != null) ? [circle] : []),
-//         onMapCreated: (GoogleMapController controller) {
-//           _controller = controller;
-//         },
 
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//           child: Icon(Icons.location_searching),
-//           onPressed: () {
-//             getCurrentLocation();
-//           }),
-//     );
-  return Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: true,
-        title: Text("Where is  ${_firebaseAuth.currentUser.displayName} ?"),
-        leading: IconButton(icon:Icon(Icons.arrow_back),
-        onPressed: () {
-          // Navigate back to the first screen by popping the current route
-          // off the stack.
-          // Navigator.pop(context);
-            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
-                        return ProfileScreen();}));
-        }
-          // onPressed: () => Navigator.of(context).pop(),
-        )
-      ),
+          automaticallyImplyLeading: true,
+          title: Text("Where is  ${_firebaseAuth.currentUser.displayName} ?"),
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                // Navigate back to the first screen by popping the current route
+                // off the stack.
+                // Navigator.pop(context);
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) {
+                  return ProfileScreen();
+                }));
+              }
+              // onPressed: () => Navigator.of(context).pop(),
+              )),
       body: GoogleMap(
         mapType: MapType.hybrid,
         initialCameraPosition: initialLocation,
