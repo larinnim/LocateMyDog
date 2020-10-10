dde6182cd7354455f93e3c6f8c3fd67ebb011bb8
larinnim <larinnimalheiros@gmail.com>
2020-10-09 22:14:50 -0400

    w

---
+++ lib/Services/bluetooth_conect.dart
@@ -0,0 +1,207 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_ble_lib/flutter_ble_lib.dart';
+import 'package:flutter_maps/Screens/Profile/profile.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:location/location.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class BluetoothConnection extends StatefulWidget {
+  @override
+  _BluetoothConnectionState createState() => _BluetoothConnectionState();
+}
+
+class _BluetoothConnectionState extends State<BluetoothConnection> {
+  BleManager _bleManager = BleManager();
+  // BluetoothState currentState = await _bleManager.bluetoothState();
+  bool _isScanning = false;
+  List<BleDeviceItem> deviceList = [];
+  List<Service> services = [];
+  // List<Characteristic> characteristics1 = [];
+
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
+
+  void init() async {
+    //BLE 생성
+    await _bleManager
+        .createClient(
+            restoreStateIdentifier: "example-restore-state-identifier",
+            restoreStateAction: (peripherals) {
+              peripherals?.forEach((peripheral) {
+                print("Restored peripheral: ${peripheral.name}");
+              });
+            })
+        .catchError((e) => print("Couldn't create BLE client  $e"))
+        .then((_) => _checkPermissions())
+        .catchError((e) => print("Permission check error $e"));
+    //.then((_) => _waitForBluetoothPoweredOn())
+  }
+
+  _checkPermissions() async {
+    if (Platform.isAndroid) {
+      if (await Permission.contacts.request().isGranted) {}
+      // Map<Permission, PermissionStatus> statuses = await [
+      //   Permission.location
+      // ].request();
+      // print(statuses[Permission.location]);
+    }
+  }
+
+  void connect(Peripheral peripheral) async {
+    peripheral.connect();
+    if (await peripheral.isConnected()) {
+      //assuming peripheral is connected
+      peripheral.discoverAllServicesAndCharacteristics(
+          transactionId: "discovery");
+      services = await peripheral.services(); //getting all services
+      setupAlertDialoadContainer();
+    }
+  }
+
+  Widget setupAlertDialoadContainer() {
+    return Container(
+      height: 300.0, // Change as per your requirement
+      width: 300.0, // Change as per your requirement
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: 5,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(services[index].uuid),
+            // title: Text('Gujarat, India'),
+          );
+        },
+      ),
+    );
+  }
+
+  //스캔 ON/OFF
+  void scan() async {
+    if (!_isScanning) {
+      deviceList.clear();
+      // _bleManager.startPeripheralScan().listen((scanResult) {
+        _bleManager.startPeripheralScan(uuids: ["4fafc201-1fb5-459e-8fcc-c5c9c331914b",],).listen((scanResult) {
+        // 페리페럴 항목에 이름이 있으면 그걸 사용하고
+        // 없다면 어드버타이지먼트 데이터의 이름을 사용하고 그것 마져 없다면 Unknown으로 표시
+        var name = scanResult.peripheral.name ??
+            scanResult.advertisementData.localName ??
+            "Unknown";
+
+        // 여러가지 정보 확인
+        print("Scanned Name $name, RSSI ${scanResult.rssi}");
+        print(
+            "\tidentifier(mac) ${scanResult.peripheral.identifier}"); //mac address
+        print("\tservice UUID : ${scanResult.advertisementData.serviceUuids}");
+        print(
+            "\tmanufacture Data : ${scanResult.advertisementData.manufacturerData}");
+        print(
+            "\tTx Power Level : ${scanResult.advertisementData.txPowerLevel}");
+        print("\t${scanResult.peripheral}");
+
+        //이미 검색된 장치인지 확인 mac 주소로 확인
+        var findDevice = deviceList.any((element) {
+          if (element.peripheral.identifier ==
+              scanResult.peripheral.identifier) {
+            //이미 존재하면 기존 값을 갱신.
+            element.peripheral = scanResult.peripheral;
+            element.advertisementData = scanResult.advertisementData;
+            element.rssi = scanResult.rssi;
+            return true;
+          }
+          return false;
+        });
+        //처음 발견된 장치라면 devicelist에 추가
+        if (!findDevice) {
+          deviceList.add(BleDeviceItem(name, scanResult.rssi,
+              scanResult.peripheral, scanResult.advertisementData));
+        }
+        //갱긴 적용.
+        setState(() {});
+      });
+      //스캔중으로 변수 변경
+      setState(() {
+        _isScanning = true;
+      });
+    } else {
+      //스캔중이었다면 스캔 정지
+      _bleManager.stopPeripheralScan();
+      setState(() {
+        _isScanning = false;
+      });
+    }
+  }
+
+  list() {
+    return ListView.builder(
+      itemCount: deviceList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          //디바이스 이름과 맥주소 그리고 신호 세기를 표시한다.
+          title: Text(deviceList[index].deviceName),
+          // subtitle: Text("${deviceList[index].advertisementData}"),
+          subtitle: Text(deviceList[index].peripheral.identifier),
+          trailing: Text("${deviceList[index].rssi}"),
+          onTap: () {
+            connect(deviceList[index].peripheral);
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Locate My Pet"),
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              // Navigate back to the first screen by popping the current route
+              // off the stack.
+              // Navigator.pop(context);
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) {
+                return ProfileScreen();
+              }));
+            }
+            // onPressed: () => Navigator.of(context).pop(),
+            ),
+      ),
+      body: Center(
+        //디바이스 리스트 함수 호출
+        child: list(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: scan, //버튼이 눌리면 스캔 ON/OFF 동작
+        child: Icon(_isScanning
+            ? Icons.stop
+            : Icons.bluetooth_searching), //_isScanning 변수에 따라 아이콘 표시 변경
+      ),
+    );
+  }
+}
+
+class BleDeviceItem {
+  String deviceName;
+  Peripheral peripheral;
+  int rssi;
+  AdvertisementData advertisementData;
+  BleDeviceItem(
+      this.deviceName, this.rssi, this.peripheral, this.advertisementData);
+}
