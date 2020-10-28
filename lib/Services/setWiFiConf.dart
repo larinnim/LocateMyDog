6dd0129fad7e2ff12f1f88f661e34c56121996d1
larinnim <larinnimalheiros@gmail.com>
2020-10-27 20:38:08 -0400

    still working

--- lib/Services/setWiFiConf.dart
+++ lib/Services/setWiFiConf.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:wifi/wifi.dart';
 import 'bluetooth_conect.dart';
 import 'dart:convert' show utf8;
 
@@ -10,11 +11,114 @@ class SetWiFiConf extends StatefulWidget {
 }
 
 class _SetWiFiConfPageState extends State<SetWiFiConf> {
+  String _wifiName = '';
+  int level = 0;
   DateTime date = DateTime.now();
   TimeOfDay time = TimeOfDay.now();
-
+  List<WifiResult> ssidList = [];
+  String ssid = '', password = '';
   TextEditingController wifiNameController = TextEditingController();
   TextEditingController wifiPasswordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  Widget itemSSID(index) {
+    if (index == 0) {
+      return Column(children: [
+        Row(
+          children: <Widget>[
+            Expanded(
+                child: Column(children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  filled: true,
+                  icon: Icon(Icons.wifi),
+                  hintText: 'Your wifi ssid',
+                  // labelText: 'ssid',
+                  labelText: _wifiName,
+                  // suffixIcon: Padding(
+                  //   //suffixIcon, this way it don't disapear when the TextField is unfocused
+                  //   padding: EdgeInsets.only(
+                  //       top: 20), //padding to put closer to the line
+                  //   child: Text(
+                  //     _wifiName,
+                  //     style: TextStyle(
+                  //       color: Colors.grey,
+                  //     ),
+                  //   ),
+                  // ),`
+                ),
+                keyboardType: TextInputType.text,
+                readOnly: true,
+                // onChanged: (value) {
+                //   ssid = value;
+                // },
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  filled: true,
+                  icon: Icon(Icons.lock_outline),
+                  hintText: 'Your wifi password',
+                  labelText: 'password',
+                ),
+                keyboardType: TextInputType.text,
+                onChanged: (value) {
+                  password = value;
+                },
+              ),
+            ])),
+          ],
+        )
+      ]);
+    } else {
+      return Column(children: <Widget>[
+        ListTile(
+          // onTap: _getWifiName(),
+          onTap: () {
+            setState(() {
+              _wifiName =  ssidList[index - 1].ssid;
+            });
+          } ,
+          leading: Image.asset('images/wifi${ssidList[index - 1].level}.png',
+              width: 28, height: 21),
+          title: Text(
+            ssidList[index - 1].ssid,
+            style: TextStyle(
+              color: Colors.black87,
+              fontSize: 16.0,
+            ),
+          ),
+          dense: true,
+        ),
+        Divider(),
+      ]);
+    }
+  }
+
+  // Future<Null> _getWifiName(String arg) async {
+  void _getWifiName(String arg) {
+    // int l = await Wifi.level;
+    // String wifiName = await Wifi.ssid;
+    setState(() {
+      // level = l;
+      _wifiName = arg;
+    });
+    // return arg;
+  }
+
+  void loadData() async {
+    Wifi.list('').then((list) {
+      setState(() {
+        ssidList = list;
+      });
+    });
+  }
 
   void submitAction() {
     var wifiData = '${wifiNameController.text},${wifiPasswordController.text}';
@@ -33,62 +137,25 @@ class _SetWiFiConfPageState extends State<SetWiFiConf> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Wi-Fi'), centerTitle: true),
-      body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Container(
-            padding: EdgeInsets.all(10.0),
-            child: Column(children: [
-              Text('WiFi SSID'),
-              SizedBox(height: 10.0),
-              TextFormField(
-                controller: wifiNameController,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                    hintText: 'Please type the Wi-Fi SSID',
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 32.0),
-                        borderRadius: BorderRadius.circular(5.0)),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                        borderRadius: BorderRadius.circular(5.0))),
-                onChanged: (value) {
-                  //Do something with this value
-                },
-              ),
-              SizedBox(height: 10.0),
-              Text('Wi-Fi Password'),
-              SizedBox(height: 10.0),
-              TextFormField(
-                controller: wifiPasswordController,
-                keyboardType: TextInputType.text,
-                obscureText: true,
-                decoration: InputDecoration(
-                    hintText: 'Please Type the WiFi Password',
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 32.0),
-                        borderRadius: BorderRadius.circular(5.0)),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                        borderRadius: BorderRadius.circular(5.0))),
-              ),
-              SizedBox(height: 50.0),
-              MaterialButton(
-                color: Colors.orange,
-                child: Text('Submit', style: TextStyle(color: Colors.white)),
-                onPressed: () {
-                  submitAction();
-                },
-              ),
-              SizedBox(height: 10.0),
-              FlatButton(
-                  color: Colors.green,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0)),
-                  onPressed: () {},
-                  child: Text("Connect")),
-            ]),
-          )),
+      appBar: AppBar(
+        title: Text('Wifi'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Column(children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              padding: EdgeInsets.all(8.0),
+              itemCount: ssidList.length + 1,
+              itemBuilder: (BuildContext context, int index) {
+                return itemSSID(index);
+              },
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
