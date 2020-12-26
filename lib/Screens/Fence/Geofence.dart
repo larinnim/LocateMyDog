4eda85e56ce893f0846b36320327bf3b87e85806
larinnim <larinnimalheiros@gmail.com>
2020-12-26 01:11:55 -0500

    working on the geofence

--- lib/Screens/Fence/Geofence.dart
+++ lib/Screens/Fence/Geofence.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:collection';
 import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -23,12 +24,35 @@ class _GeofenceWidgetState extends State<Geofence> {
   localization.Location _locationTracker = localization.Location();
   Circle circle;
   Marker marker;
+  Polygon polygon;
   GoogleMapController _controller;
   static LatLng _initialPosition;
   var _configuredRadius = 0.0; //Radius is 30 meters
   AppUser _currentUser = locator.get<UserController>().currentUser;
   CollectionReference locateDogInstance =
       FirebaseFirestore.instance.collection('locateDog');
+  bool _isPolygonFence = false;
+  bool _isDoNotEnterFence = false;
+  Set<Polygon> _polygonsFence = HashSet<Polygon>();
+  List<LatLng> polygonFenceLatLngs = List<LatLng>();
+  int _polygonFenceIdCounter = 1;
+  Set<Polygon> _doNotEnterFence = HashSet<Polygon>();
+  List<LatLng> dotNotEnterFenceLatLngs = List<LatLng>();
+  int _doNotEnterFenceIdCounter = 1;
+  // List<Polygon> polygons = <Polygon>[
+  //   new Polygon(
+  // polygonId: PolygonId('area'),
+  // points: <LatLng>[
+  //   new LatLng(46.52098670361095, -80.95421220237156),
+  //   new LatLng(46.52101623317156, -80.95524753496112),
+  //   new LatLng(46.52058805297215, -80.95529581471918),
+  //   new LatLng(46.52024845938013, -80.95401908333933)
+  // ],
+  // geodesic: true,
+  // strokeColor: Colors.blue,
+  // fillColor: Colors.lightBlue.withOpacity(0.1),
+  //   )
+  // ];
 
   @override
   void initState() {
@@ -112,6 +136,30 @@ class _GeofenceWidgetState extends State<Geofence> {
     _initialPosition = LatLng(position.latitude, position.longitude);
   }
 
+  void _setPolygonFence() {
+    final String polygonVal = 'polygon_id_$_polygonFenceIdCounter';
+    _polygonsFence.add(Polygon(
+      polygonId: PolygonId(polygonVal),
+      points: polygonFenceLatLngs,
+      // geodesic: true,
+      strokeWidth: 2,
+      strokeColor: Colors.blue,
+      fillColor: Colors.lightBlue,
+    ));
+  }
+
+  void _setDoNotEnterFence() {
+    final String doNotEnterVal = 'doNotEnter_id_$_doNotEnterFenceIdCounter';
+    _doNotEnterFence.add(Polygon(
+      polygonId: PolygonId(doNotEnterVal),
+      points: dotNotEnterFenceLatLngs,
+      // geodesic: true,
+      strokeWidth: 2,
+      strokeColor: Colors.red,
+      fillColor: Colors.redAccent,
+    ));
+  }
+
   void updateMarkerAndCircle(
       localization.LocationData newLocalData, Uint8List imageData) {
     LatLng latlng = LatLng(newLocalData.latitude, newLocalData.longitude);
@@ -182,17 +230,35 @@ class _GeofenceWidgetState extends State<Geofence> {
             ),
           ],
         ),
-        onTap: () => {},
+        onTap: () => {
+          mystate(() {
+            _isPolygonFence = false;
+            _isDoNotEnterFence = false;
+          }),
+          Navigator.of(context).pop()
+        },
       ),
       ListTile(
         leading: Icon(FontAwesomeIcons.drawPolygon),
         title: Text("Polygon Geofence"),
-        onTap: () => {},
+        onTap: () => {
+          mystate(() {
+            _isPolygonFence = true;
+            _isDoNotEnterFence = false;
+          }),
+          Navigator.of(context).pop()
+        },
       ),
       ListTile(
         leading: Icon(Icons.do_disturb_on_outlined),
         title: Text("Dot Not Enter Area"),
-        onTap: () => {},
+        onTap: () => {
+          mystate(() {
+            _isPolygonFence = false;
+            _isDoNotEnterFence = true;
+          }),
+          Navigator.of(context).pop()
+        },
       )
     ]);
   }
@@ -252,6 +318,25 @@ class _GeofenceWidgetState extends State<Geofence> {
               myLocationButtonEnabled: false,
               markers: Set.of((marker != null) ? [marker] : []),
               circles: Set.of((circle != null) ? [circle] : []),
+              polygons: _isPolygonFence
+                  ? _polygonsFence
+                  : _isDoNotEnterFence
+                      ? _doNotEnterFence
+                      : null,
+              onTap: (point) {
+                if (_isPolygonFence) {
+                  setState(() {
+                    polygonFenceLatLngs.add(point);
+                    _setPolygonFence();
+                  });
+                }
+                if (_isDoNotEnterFence) {
+                  setState(() {
+                    dotNotEnterFenceLatLngs.add(point);
+                    _setDoNotEnterFence();
+                  });
+                }
+              },
               onMapCreated: (GoogleMapController controller) {
                 _controller = controller;
               },
