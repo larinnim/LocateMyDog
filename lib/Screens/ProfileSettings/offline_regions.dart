3ae296e0a5160c9359d376849de2edce6a1404d5
larinnim <larinnimalheiros@gmail.com>
2021-03-17 19:23:08 -0400

    Added download map feature

--- lib/Screens/ProfileSettings/offline_regions.dart
+++ lib/Screens/ProfileSettings/offline_regions.dart
@@ -16,41 +16,41 @@ places.GoogleMapsPlaces _places =
 
 // final String defaultLocale = Get.deviceLocale.toLanguageTag();
 
-final LatLngBounds hawaiiBounds = LatLngBounds(
-  southwest: const LatLng(17.26672, -161.14746),
-  northeast: const LatLng(23.76523, -153.74267),
-);
+// final LatLngBounds hawaiiBounds = LatLngBounds(
+//   southwest: const LatLng(17.26672, -161.14746),
+//   northeast: const LatLng(23.76523, -153.74267),
+// );
 
-final LatLngBounds santiagoBounds = LatLngBounds(
-  southwest: const LatLng(-33.5597, -70.49102),
-  northeast: const LatLng(-33.33282, -153.74267),
-);
+// final LatLngBounds santiagoBounds = LatLngBounds(
+//   southwest: const LatLng(-33.5597, -70.49102),
+//   northeast: const LatLng(-33.33282, -153.74267),
+// );
 
-final LatLngBounds aucklandBounds = LatLngBounds(
-  southwest: const LatLng(-36.87838, 174.73205),
-  northeast: const LatLng(-36.82838, 174.79745),
-);
+// final LatLngBounds aucklandBounds = LatLngBounds(
+//   southwest: const LatLng(-36.87838, 174.73205),
+//   northeast: const LatLng(-36.82838, 174.79745),
+// );
 
-final List<OfflineRegionDefinition> regionDefinitions = [
-  OfflineRegionDefinition(
-    bounds: hawaiiBounds,
-    minZoom: 3.0,
-    maxZoom: 8.0,
-    mapStyleUrl: MapboxStyles.MAPBOX_STREETS,
-  ),
-  OfflineRegionDefinition(
-    bounds: santiagoBounds,
-    minZoom: 10.0,
-    maxZoom: 16.0,
-    mapStyleUrl: MapboxStyles.MAPBOX_STREETS,
-  ),
-  OfflineRegionDefinition(
-    bounds: aucklandBounds,
-    minZoom: 13.0,
-    maxZoom: 16.0,
-    mapStyleUrl: MapboxStyles.MAPBOX_STREETS,
-  ),
-];
+// final List<OfflineRegionDefinition> regionDefinitions = [
+//   OfflineRegionDefinition(
+//     bounds: hawaiiBounds,
+//     minZoom: 3.0,
+//     maxZoom: 8.0,
+//     mapStyleUrl: MapboxStyles.MAPBOX_STREETS,
+//   ),
+//   OfflineRegionDefinition(
+//     bounds: santiagoBounds,
+//     minZoom: 10.0,
+//     maxZoom: 16.0,
+//     mapStyleUrl: MapboxStyles.MAPBOX_STREETS,
+//   ),
+//   OfflineRegionDefinition(
+//     bounds: aucklandBounds,
+//     minZoom: 13.0,
+//     maxZoom: 16.0,
+//     mapStyleUrl: MapboxStyles.MAPBOX_STREETS,
+//   ),
+// ];
 
 final List<String> regionNames = ['Hawaii', 'Santiago', 'Auckland'];
 
@@ -60,14 +60,14 @@ class OfflineRegionListItem {
     @required this.downloadedId,
     @required this.isDownloading,
     @required this.name,
-    @required this.estimatedTiles,
+    // @required this.estimatedTiles,
   });
 
   final OfflineRegionDefinition offlineRegionDefinition;
   final int downloadedId;
   final bool isDownloading;
   final String name;
-  final int estimatedTiles;
+  // final int estimatedTiles;
 
   OfflineRegionListItem copyWith({
     int downloadedId,
@@ -76,7 +76,7 @@ class OfflineRegionListItem {
       OfflineRegionListItem(
         offlineRegionDefinition: offlineRegionDefinition,
         name: name,
-        estimatedTiles: estimatedTiles,
+        // estimatedTiles: estimatedTiles,
         downloadedId: downloadedId,
         isDownloading: isDownloading ?? this.isDownloading,
       );
@@ -84,29 +84,29 @@ class OfflineRegionListItem {
   bool get isDownloaded => downloadedId != null;
 }
 
-final List<OfflineRegionListItem> allRegions = [
-  OfflineRegionListItem(
-    offlineRegionDefinition: regionDefinitions[0],
-    downloadedId: null,
-    isDownloading: false,
-    name: regionNames[0],
-    estimatedTiles: 61,
-  ),
-  OfflineRegionListItem(
-    offlineRegionDefinition: regionDefinitions[1],
-    downloadedId: null,
-    isDownloading: false,
-    name: regionNames[1],
-    estimatedTiles: 3580,
-  ),
-  OfflineRegionListItem(
-    offlineRegionDefinition: regionDefinitions[2],
-    downloadedId: null,
-    isDownloading: false,
-    name: regionNames[2],
-    estimatedTiles: 202,
-  ),
-];
+// final List<OfflineRegionListItem> allRegions = [
+// OfflineRegionListItem(
+//   offlineRegionDefinition: regionDefinitions[0],
+//   downloadedId: null,
+//   isDownloading: false,
+//   name: regionNames[0],
+//   // estimatedTiles: 61,
+// ),
+//   OfflineRegionListItem(
+//     offlineRegionDefinition: regionDefinitions[1],
+//     downloadedId: null,
+//     isDownloading: false,
+//     name: regionNames[1],
+//     // estimatedTiles: 3580,
+//   ),
+//   OfflineRegionListItem(
+//     offlineRegionDefinition: regionDefinitions[2],
+//     downloadedId: null,
+//     isDownloading: false,
+//     name: regionNames[2],
+//     // estimatedTiles: 202,
+//   ),
+// ];
 
 class OfflineRegionBody extends StatefulWidget {
   const OfflineRegionBody();
@@ -116,8 +116,7 @@ class OfflineRegionBody extends StatefulWidget {
 }
 
 class _OfflineRegionsBodyState extends State<OfflineRegionBody> {
-  List<OfflineRegionListItem> _items = List();
-  final TextEditingController _controller = new TextEditingController();
+  List<OfflineRegionListItem> _items = [];
 
   bool isLoading = false;
   @override
@@ -147,7 +146,7 @@ class _OfflineRegionsBodyState extends State<OfflineRegionBody> {
 
       print(lat);
       print(lng);
-      Navigator.of(context).push(
+      Navigator.of(context).pushReplacement(
         MaterialPageRoute<void>(
           builder: (_) => OfflineRegionMapSelection(
             offlineRegion: OfflineMapRegion(LatLng(lat, lng)),
@@ -188,64 +187,6 @@ class _OfflineRegionsBodyState extends State<OfflineRegionBody> {
                           Get.deviceLocale.countryCode)
                     ]);
                 displayPrediction(p);
-                // showCupertinoDialog(
-                //   context: context,
-                //   builder: (_) => CupertinoAlertDialog(
-                //     title: new Text("Type the address\n"),
-                //     content: CupertinoTextField(
-                //       prefix: Padding(
-                //         padding: EdgeInsets.all(8.0),
-                //       ),
-                //       clearButtonMode: OverlayVisibilityMode.editing,
-                //       controller: _controller,
-                //       placeholder: "Type the address",
-                //     ),
-                // actions: <Widget>[
-                // CupertinoDialogAction(
-                //   isDefaultAction: true,
-                //   child: Text("Cancel"),
-                //   onPressed: () => Navigator.pop(context),
-                // ),
-                // CupertinoDialogAction(
-                //   child: Text("Search"),
-                //   onPressed: () async {
-                //     places.Prediction p = await PlacesAutocomplete.show(
-                //         context: context, apiKey: kGoogleApiKey);
-                //     displayPrediction(p);
-                //   },
-                // )
-                // ],
-
-                // title: Text('TextField in Dialog'),
-                // content: TextField(
-                //   controller: null,
-                //   decoration:
-                //       InputDecoration(hintText: "Text Field in Dialog"),
-                // ),
-                // actions: <Widget>[
-                //   FlatButton(
-                //     child: Text('CANCEL'),
-                //     onPressed: () {
-                //       Navigator.pop(context);
-                //     },
-                //   ),
-                //   FlatButton(
-                //     child: Text('OK'),
-                //     onPressed: () {
-                //       // print(_textFieldController.text);
-                //       Navigator.pop(context);
-                //     },
-                //   ),
-                // ],
-                //   ),
-                // );
-
-                // Navigator.of(context).push(MaterialPageRoute(
-                //   builder: (context) =>
-                //   OfflineRegionMapSelection(),
-                // ));
-
-                // _onSettingsPressed();
               },
               child: Icon(Icons.add_circle_outline),
             ),
@@ -284,12 +225,12 @@ class _OfflineRegionsBodyState extends State<OfflineRegionBody> {
                         fontSize: 18,
                       ),
                     ),
-                    Text(
-                      'Est. tiles: ${_items[index].estimatedTiles}',
-                      style: TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
+                    // Text(
+                    //   'Est. tiles: ${_items[index].estimatedTiles}',
+                    //   style: TextStyle(
+                    //     fontSize: 16,
+                    //   ),
+                    // ),
                   ],
                 ),
                 const Spacer(),
@@ -317,57 +258,57 @@ class _OfflineRegionsBodyState extends State<OfflineRegionBody> {
     );
   }
 
-  // _showDialog() async {
-  //   await showDialog<String>(
-  //     context: context,
-  //     child: new AlertDialog(
-  //       contentPadding: const EdgeInsets.all(16.0),
-  //       content: new Row(
-  //         children: <Widget>[
-  //           new Expanded(
-  //             child: new TextField(
-  //               autofocus: true,
-  //               decoration: new InputDecoration(
-  //                   labelText: 'Full Name', hintText: 'eg. John Smith'),
-  //             ),
-  //           )
-  //         ],
-  //       ),
-  //       actions: <Widget>[
-  //         new FlatButton(
-  //             child: const Text('CANCEL'),
-  //             onPressed: () {
-  //               Navigator.pop(context);
-  //             }),
-  //         new FlatButton(
-  //             child: const Text('OPEN'),
-  //             onPressed: () {
-  //               Navigator.pop(context);
-  //             })
-  //       ],
-  //     ),
-  //   );
-  // }
-
   void _updateListOfRegions() async {
     List<OfflineRegion> offlineRegions = await getListOfRegions(
         accessToken:
             "pk.eyJ1IjoibGFyaW5uaW1hbGhlaXJvcyIsImEiOiJja200M2s2NmQwMHQwMnZwdTUxZng1enFrIn0.ZeWhg3_t_0o4QOQooXf-9w");
     List<OfflineRegionListItem> regionItems = [];
-    for (var item in allRegions) {
-      final offlineRegion = offlineRegions.firstWhere(
-          (offlineRegion) => offlineRegion.metadata['name'] == item.name,
-          orElse: () => null);
-      if (offlineRegion != null) {
-        regionItems.add(item.copyWith(downloadedId: offlineRegion.id));
-      } else {
-        regionItems.add(item);
-      }
+
+    for (var region in offlineRegions) {
+      regionItems.add(OfflineRegionListItem(
+        offlineRegionDefinition: OfflineRegionDefinition(
+          bounds: region.definition.bounds,
+          minZoom: region.definition.minZoom,
+          maxZoom: region.definition.maxZoom,
+          mapStyleUrl: region.definition.mapStyleUrl,
+        ),
+        downloadedId: region.id,
+        isDownloading: false,
+        name: region.metadata['name'],
+      ));
     }
+    // setState(() {
+    // offlineRegions.map((region) {
+    //   _items.add(OfflineRegionListItem(
+    //     offlineRegionDefinition: OfflineRegionDefinition(
+    //       bounds: region.definition.bounds,
+    //       minZoom: region.definition.minZoom,
+    //       maxZoom: region.definition.maxZoom,
+    //       mapStyleUrl: region.definition.mapStyleUrl,
+    //     ),
+    //     downloadedId: region.id,
+    //     isDownloading: false,
+    //     name: region.metadata['name'],
+    //   ));
+    // });
+    // });
+    // for (var item in allRegions) {
+    //   final offlineRegion = offlineRegions.firstWhere(
+    //       (offlineRegion) => offlineRegion.metadata['name'] == item.name,
+    //       orElse: () => null);
+    //   if (offlineRegion != null) {
+    //     regionItems.add(item.copyWith(downloadedId: offlineRegion.id));
+    //   } else {
+    //     regionItems.add(item);
+    //   }
+    // }
     setState(() {
       _items.clear();
       _items.addAll(regionItems);
     });
+    // setState(() {
+    //   _items;
+    // });
   }
 
   void _downloadRegion(OfflineRegionListItem item, int index) async {
@@ -432,18 +373,10 @@ class _OfflineRegionsBodyState extends State<OfflineRegionBody> {
   }
 
   _goToMap(OfflineRegionListItem item) {
-    Navigator.of(context).push(
+    Navigator.of(context).pushReplacement(
       MaterialPageRoute<void>(
         builder: (_) => OfflineRegionMap(item),
       ),
     );
   }
-
-  // _goToDownloadMap() {
-  //   Navigator.of(context).push(
-  //     MaterialPageRoute<void>(
-  //       builder: (_) => OfflineRegionMapSelection(offlineRegion: null,),
-  //     ),
-  //   );
-  // }
 }
--- lib/Screens/ProfileSettings/offline_regions_section.dart
+++ lib/Screens/ProfileSettings/offline_regions_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
+import './offline_regions.dart';
 
 class OfflineMapRegion {
   final LatLng latitude_longitude;
@@ -19,6 +20,17 @@ class OfflineRegionMapSelection extends StatefulWidget {
 }
 
 class _OfflineRegionMapSelectionState extends State<OfflineRegionMapSelection> {
+  MapboxMapController mapController;
+  int _numberOfRegions = 0;
+  LatLngBounds mapBounds;
+  CameraPosition _position;
+
+  @override
+  void initState() {
+    super.initState();
+    _getListOfRegions();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,6 +46,8 @@ class _OfflineRegionMapSelectionState extends State<OfflineRegionMapSelection> {
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height * 0.8,
                 child: MapboxMap(
+                  onMapCreated: onMapCreated,
+                  trackCameraPosition: true,
                   initialCameraPosition: CameraPosition(
                     target: widget.offlineRegion.latitude_longitude,
                     zoom: 12,
@@ -69,9 +83,103 @@ class _OfflineRegionMapSelectionState extends State<OfflineRegionMapSelection> {
                           RoundedRectangleBorder(
                               borderRadius: BorderRadius.zero,
                               side: BorderSide(color: Colors.red[300])))),
-                  onPressed: () => null),
+                  onPressed: () {
+                    _downloadRegion();
+                  }),
             ),
           ],
         ));
+  }
+
+  void onMapCreated(MapboxMapController controller) {
+    mapController = controller;
+    mapController.addListener(_onMapChanged);
+    _extractMapInfo();
+  }
+
+  void _onMapChanged() {
+    setState(() {
+      _extractMapInfo();
+    });
+  }
+
+  void _extractMapInfo() {
+    _position = mapController.cameraPosition;
+
+    if (mapController.isCameraMoving) {
+      mapController.getVisibleRegion().then((newLatLngBounds) {
+        mapBounds = newLatLngBounds;
+
+        // double n_lat = newLatLngBounds.northeast.latitude;
+        // double n_lng = newLatLngBounds.northeast.longitude;
+        // double s_lat = newLatLngBounds.southwest.latitude;
+        // double s_lng = newLatLngBounds.southwest.longitude;
+
+        // mapController.moveCamera(CameraUpdate.newLatLngBounds(LatLngBounds(
+        //     southwest: LatLng(s_lat, s_lng), northeast: LatLng(n_lat, n_lng))));
+      });
+    }
+  }
+
+  void _downloadRegion() async {
+    // setState(() {
+    //   _items.removeAt(index);
+    //   _items.insert(index, item.copyWith(isDownloading: true));
+    // });
+
+    try {
+      final downloadingRegion = await downloadOfflineRegion(
+        OfflineRegionDefinition(
+          bounds: mapBounds,
+          minZoom: _position.zoom - 1,
+          maxZoom: _position.zoom + 1,
+          mapStyleUrl: MapboxStyles.MAPBOX_STREETS,
+        ),
+        metadata: {
+          'name': 'Map ' + _numberOfRegions.toString(),
+        },
+        accessToken:
+            "pk.eyJ1IjoibGFyaW5uaW1hbGhlaXJvcyIsImEiOiJja200M2s2NmQwMHQwMnZwdTUxZng1enFrIn0.ZeWhg3_t_0o4QOQooXf-9w",
+      );
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute<void>(
+          builder: (_) => OfflineRegionBody(),
+        ),
+      );
+      // setState(() {
+      //   _items.removeAt(index);
+      //   _items.insert(
+      //       index,
+      //       item.copyWith(
+      //         isDownloading: false,
+      //         downloadedId: downloadingRegion.id,
+      //       ));
+      // });
+    } on Exception catch (exception) {
+      print(exception.toString());
+
+      // setState(() {
+      //   _items.removeAt(index);
+      //   _items.insert(
+      //       index,
+      //       item.copyWith(
+      //         isDownloading: false,
+      //         downloadedId: null,
+      //       ));
+      // });
+      return;
+    } catch (error) {
+      print(error.toString());
+      // executed for errors of all types other than Exception
+    }
+  }
+
+  void _getListOfRegions() async {
+    List<OfflineRegion> offlineRegions = await getListOfRegions(
+        accessToken:
+            "pk.eyJ1IjoibGFyaW5uaW1hbGhlaXJvcyIsImEiOiJja200M2s2NmQwMHQwMnZwdTUxZng1enFrIn0.ZeWhg3_t_0o4QOQooXf-9w");
+    setState(() {
+      _numberOfRegions = offlineRegions.length;
+    });
   }
 }
